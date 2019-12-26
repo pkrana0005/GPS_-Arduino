dba5b37f63153d038487ddc75f259c92b0b2e2b7
Pawan Kumar <pkrana0005@gmail.com>
2019-12-26 14:24:27 +0530

    GPS_Raw_data_in_NMEA_format
    USED Node MCU

---
+++ Raw_data_NMEA_format_/Raw_data_NMEA_format_.ino
@@ -0,0 +1,15 @@
+#include <SoftwareSerial.h>   //using "SoftwareSerial" library
+SoftwareSerial ss(D8,D7); //object name is "ss" here and pin defined as Rx,Tx respectively
+void setup() {         // put your setup code here, to run once:
+ Serial.begin(9600);   //intialising serial communication with USB at 9600 bits/sec
+ ss.begin(9600);       //intialising serial communication with GPS module at 9600 bits/sec
+}
+
+void loop() {                 // put your main code here, to run repeatedly:
+ while(ss.available()>0)       //while GPS data is ready to be send to NodeMCU
+    {
+      byte dataGPS = ss.read();  //reading GPS data and saving it to variable "dataGPS" (data type is "byte")
+      Serial.write(dataGPS);     //printing the RAW GPS data on serial monitor
+      
+      }
+}
